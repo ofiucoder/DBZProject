c8045ac42e77e0d64a5a8ecf56552f30b4c97c2c
Alberto Casas <alberto.casas.ofiuco@gmail.com>
2024-10-18 22:18:20 +0200

    Liado: archivos no aparencen en la rama planets

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const requestDBZPlanetsURL = "https://dragonball-api.com/api/planets/?limit=20"
+
+  
+
+async function fetchPlanetsJson() {
+  const response = await fetch(requestDBZPlanetsURL)
+  const data = await response.json()
+  const dbzPlanets = data.items
+  return dbzPlanets
+}
+
+  let container = document.getElementById("container")
+  fetchPlanetsJson().then(dbzPlanets =>{
+      dbzPlanets.forEach(element => {
+        container.innerHTML += `
+        <div id="dbzPlanetItem">
+        <div class="${element.name}">id.${element.id} </div>
+        <img src="${element.image}" class="image" alt="Foto de planeta">
+        <p>${element.name}</p>
+        <p>${element.description}</p>
+        <a>${element.isDestroyed}</a>
+        </div>
+    `
+      });
+  })
+
+
+/*
+    for (let index = 0; index < items.length; index++) {
+        let idPlanet = items.dbzPlanets[index].id
+        let namePlanet = items.dbzPlanets[index].name
+        let isDestroyedPlanet = items.dbzPlanets[index].isDestroyed
+        let descriptionPlanet = items.dbzPlanets[index].description
+        let imagePlanet = items.dbzPlanets[index].image
+        let dbzPlanet = [idPlanet,namePlanet,isDestroyedPlanet,descriptionPlanet,imagePlanet]
+    }
+
+
+    let container = document.getElementById("container")
+    })
+*/
+
