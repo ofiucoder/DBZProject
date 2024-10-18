1cd32cb431f9ecab6fbf0b56b738afde6c879cbb
Alberto Casas <alberto.casas.ofiuco@gmail.com>
2024-10-18 22:20:51 +0200

    change: se cambia el nombre de los arhivos, lio resuelto

---
+++ js/planets.js
@@ -0,0 +1,27 @@
+const requestDBZPlanetsURL = "https://dragonball-api.com/api/planets/?limit=20"
+
+
+async function fetchPlanetsJson() {
+  const response = await fetch(requestDBZPlanetsURL)
+  const data = await response.json()
+  const dbzPlanets = data.items
+  return dbzPlanets
+}
+
+  let container = document.getElementById("container")
+  fetchPlanetsJson().then(dbzPlanets =>{
+      dbzPlanets.forEach(element => {
+        container.innerHTML += `
+        <div id="dbzPlanetItem">
+        <div class="${element.name}">id.${element.id} </div>
+        <img src="${element.image}" class="image" alt="Foto de planeta">
+        <p>${element.name}</p>
+        <p>${element.description}</p>
+        <a>${element.isDestroyed}</a>
+        </div>
+    `
+      });
+  })
+
+
+
